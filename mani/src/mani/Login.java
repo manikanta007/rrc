0ac448d7948fd792a652ece42f3627ad6692bd80
joshy <joshy@mcjoshy>
2016-03-08 21:47:43 +0530

    new class is added

---
+++ mani/src/mani/Login.java
@@ -0,0 +1,11 @@
+package mani;
+
+public class Login {
+
+	public void userLogin() {
+		// TODO Auto-generated method stub
+	System.out.println("user login");	
+	}
+
+	
+}
--- mani/src/mani/MainClass.java
+++ mani/src/mani/MainClass.java
@@ -5,5 +5,9 @@ public class MainClass {
 	public static void main(String...strings ) {
 		
 		System.out.println("welcome");
+		
+		
+		Login login= new Login();
+		login.userLogin();
 	}
 }
